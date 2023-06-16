0744794aa3d635b189a5d3824f542f3bca296edb
yukselibrahim <113780430+yukselibrahim@users.noreply.github.com>
2023-06-16 03:24:37 +0300

    add powerbar functions

--- Don't Touch My Box2D/Assets/Scripts/Oyuncu.cs
+++ Don't Touch My Box2D/Assets/Scripts/Oyuncu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Oyuncu : MonoBehaviour
 {
@@ -9,10 +10,45 @@ public class Oyuncu : MonoBehaviour
     [SerializeField] GameObject bombaCikisnoktasi;
     [SerializeField] ParticleSystem topAtisefekt;
     [SerializeField] AudioSource topAtmasesi;
+
+    [SerializeField] Image Powerbar;
+    float powerSayi;
+    bool sonageldimi = false;
+
+    Coroutine powerDongu;
    
     void Start()
     {
-        
+        powerDongu = StartCoroutine(PowerbarCalistir());
+    }
+
+    IEnumerator PowerbarCalistir()
+    {
+        Powerbar.fillAmount = 0;
+        sonageldimi = false;
+
+        while (true )
+        {
+            if (Powerbar.fillAmount<1 && !sonageldimi)
+            {
+                powerSayi = 0.01f;
+                Powerbar.fillAmount += powerSayi;
+                yield return new WaitForSeconds(0.0001f* Time.deltaTime);
+
+            }
+            else
+            {
+                sonageldimi = true;
+                powerSayi = 0.01f;
+                Powerbar.fillAmount -= powerSayi;
+                yield return new WaitForSeconds(0.0001f * Time.deltaTime);
+
+                if (Powerbar.fillAmount ==0)
+                {
+                    sonageldimi = false;
+                }
+            }
+        }
     }
 
    
@@ -22,11 +58,32 @@ public class Oyuncu : MonoBehaviour
         {
             GameObject bombaObjem = Instantiate(bomba, bombaCikisnoktasi.transform.position,bombaCikisnoktasi.transform.rotation);
             Rigidbody2D rg = bombaObjem.GetComponent<Rigidbody2D>();
-            rg.AddForce(new Vector2(2f, 0f) * 8f, ForceMode2D.Impulse);
+            rg.AddForce(new Vector2(2f, 0f) * Powerbar.fillAmount* 12f, ForceMode2D.Impulse);
+
+            //Powerbar.fillAmount 1 * 12 // 0.5 * 12 = 6
+            StopCoroutine(powerDongu);
             topAtisefekt.Play(); //Yukarýda particlesystem olarak tanýmladýðýmýz için topAtisefekt i play getirip oynatabiliyoruz. 
 
             Instantiate(topAtisefekt, bombaCikisnoktasi.transform.position, bombaCikisnoktasi.transform.rotation); //Burada bomba çýkýþ noktasýna patlama efektini instantiate ettik.
             topAtmasesi.Play();
         }
+
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    PowerOynasinmi = false;
+        //    Debug.Log("Gelen deðer: " + Powerbar.fillAmount);
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    PowerOynasinmi = true;
+        //    StartCoroutine(PowerbarCalistir());
+        //}
+    }
+
+    public void PowerOynasin()
+    {
+
+        powerDongu = StartCoroutine(PowerbarCalistir());
     }
 }
--- Don't Touch My Box2D/Assets/Scripts/bomba.cs
+++ Don't Touch My Box2D/Assets/Scripts/bomba.cs
@@ -8,6 +8,8 @@ public class bomba : MonoBehaviour
     [SerializeField] GameObject topYokolmaefekt;
     [SerializeField] AudioSource yokOlmasesi;
     [SerializeField] GameObject gameKontrol;
+    [SerializeField] GameObject Oyuncu;
+
 
     ortadaki_kutu ortadaki_kutularin_yönetimi;
 
@@ -15,6 +17,7 @@ public class bomba : MonoBehaviour
     {
         darbeGucu = 20f;
         gameKontrol = GameObject.FindWithTag("GameKontrol");
+        Oyuncu = GameObject.FindWithTag("Oyuncu1");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,26 +28,37 @@ public class bomba : MonoBehaviour
 
             gameKontrol.GetComponent<GameKontrol>().Ses_ve_Efekt_Olustur(1,collision.gameObject);
 
+            Oyuncu.GetComponent<Oyuncu>().PowerOynasin();
+
             Destroy(gameObject);
             
         }
 
-        if (collision.gameObject.CompareTag("Oyuncu2Kule"))
+        if (collision.gameObject.CompareTag("Oyuncu2Kule") || collision.gameObject.CompareTag("Oyuncu2"))
         {
 
             gameKontrol.GetComponent<GameKontrol>().Ses_ve_Efekt_Olustur(1, collision.gameObject);
             gameKontrol.GetComponent<GameKontrol>().Darbe_vur(2,darbeGucu);
-
+            Oyuncu.GetComponent<Oyuncu>().PowerOynasin();
             Destroy(gameObject);
            
         }
 
-        if (collision.gameObject.CompareTag("Oyuncu1Kule"))
+        if (collision.gameObject.CompareTag("Oyuncu1Kule") || collision.gameObject.CompareTag("Oyuncu1"))
         {
 
             gameKontrol.GetComponent<GameKontrol>().Ses_ve_Efekt_Olustur(1, collision.gameObject);
             gameKontrol.GetComponent<GameKontrol>().Darbe_vur(1, darbeGucu);
+            Oyuncu.GetComponent<Oyuncu>().PowerOynasin();
+            Destroy(gameObject);
 
+        }
+
+        if (collision.gameObject.CompareTag("Zemin"))
+        {
+
+            gameKontrol.GetComponent<GameKontrol>().Ses_ve_Efekt_Olustur(1, collision.gameObject);
+            Oyuncu.GetComponent<Oyuncu>().PowerOynasin();
             Destroy(gameObject);
 
         }
